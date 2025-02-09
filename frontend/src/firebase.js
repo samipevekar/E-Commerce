524ae25a880704e6599c911694c3c8178efbdb05
samipevekar <samipevekar499@gmail.com>
2025-02-09 12:48:27 +0530

    some ui changes

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -7,7 +7,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyCfewyKLRMcqKFjsPJuEe1Jt4WbH13qDIA",
   authDomain: "ecommerce-ef1c6.firebaseapp.com",
   projectId: "ecommerce-ef1c6",
   storageBucket: "ecommerce-ef1c6.firebasestorage.app",
